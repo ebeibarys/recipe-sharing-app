f1d26d8c731d7ca9bcc8abc63f0d33eca53f2d36
Alisher <sotasakurai1@gmail.com>
2026-04-24 21:14:52 +0500

    Backend: CRUD API for Recipe working

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
-from .views import test_api
+from .views import (
+    test_api,
+    category_list,
+    register_user,
+    RecipeListCreateView,
+    RecipeDetailView,
+    CommentCreateView
+)
 
 urlpatterns = [
     path('test/', test_api),
+
+    path('categories/', category_list),
+    path('register/', register_user),
+
+    path('recipes/', RecipeListCreateView.as_view()),
+    path('recipes/<int:pk>/', RecipeDetailView.as_view()),
+
+    path('comments/', CommentCreateView.as_view()),
 ]
